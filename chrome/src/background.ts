c866cce915ded157cc725f6a3c939a002c514cc8
Nikita <hormold@gmail.com>
2023-02-10 01:08:29 +0100

    use hosted version, image

--- chrome/src/background.ts
+++ chrome/src/background.ts
@@ -1,7 +1,7 @@
 console.log("hello background");
 import { ChromeRuntimeMessage } from './types/base';
 import { SHA1 } from './utils/sha1';
-const isProdMode = 'update_url' in chrome.runtime.getManifest()
+const isProdMode = true;// 'update_url' in chrome.runtime.getManifest()
 const APP_URL = !isProdMode?'http://127.0.0.1:5000':'https://ask-the-page.herokuapp.com';
 
 
